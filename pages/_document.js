a3c0337ffbd0394f65e5504c4bb1c00e7594c260
PercyPham <hungpm.percy@gmail.com>
2018-12-09 21:48:08 +0700

    chore: config to make absolute imports possible

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Document, { Head, Main, NextScript } from "next/document";
-import { BASE_URL } from "../utils/Urls";
+import { BASE_URL } from "utils/Urls";
 
 class MyDocument extends Document {
   render() {
